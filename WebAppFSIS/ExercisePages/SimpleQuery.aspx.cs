8d76fca154c444bd5a307273c92985f162683522
Eronielle Miranda <emiranda4@nait.ca>
2020-03-12 14:35:45 -0600

    Part 2 and Part 3

--- WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
+++ WebAppFSIS/ExercisePages/SimpleQuery.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FSISSystem.BLL;
+using FSISSystem.DAL;
+using FSISSystem.ENTITIES;
 
 namespace WebAppFSIS.ExercisePages
 {
@@ -11,7 +14,47 @@ namespace WebAppFSIS.ExercisePages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            MessageLabel.Text = "";
+        }
+        protected void Fetch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(TeamIDArg.Text))
+            {
+                MessageLabel.Text = "Enter a region id value.";
+            }
+            else
+            {
+                int teamid = 0;
+                if (int.TryParse(TeamIDArg.Text, out teamid))
+                {
+                    if (teamid > 0)
+                    {
+                        TeamController sysmgr = new TeamController();
+                        Team info = null;
+                        info = sysmgr.Teams_FindByID(teamid); //BLL controller method
+                        if (info == null)
+                        {
+                            MessageLabel.Text = "Region ID not found.";
+                            TeamID.Text = "";
+                            TeamName.Text = "";
+                        }
+                        else
+                        {
+                            TeamID.Text = info.TeamID.ToString();
+                            TeamName.Text = info.TeamName;
+                        }
+                    }
+                    else
+                    {
+                        MessageLabel.Text = "Region id must be greater than 0";
+                    }
 
+                }
+                else
+                {
+                    MessageLabel.Text = "Region id must be a number.";
+                }
+            }
         }
     }
 }
